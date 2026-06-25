8f18235e1e4ce8715aa9954a9f5d1abeac78e151
Riki Nakamura <riki.nakamura.edu@gmail.com>
2026-06-25 18:25:27 +0900

    docs(shell): make opt-in examples safe to enable

--- home-modules/user/zsh.nix
+++ home-modules/user/zsh.nix
@@ -19,32 +19,35 @@ in
       fi
     '';
 
-    initContent = lib.mkBefore ''
-      if [ -f "${isslConfigHome}/zsh/.zshrc" ]; then
-        . "${isslConfigHome}/zsh/.zshrc"
-      fi
-    '';
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        if [ -f "${isslConfigHome}/zsh/.zshrc" ]; then
+          . "${isslConfigHome}/zsh/.zshrc"
+        fi
+      '')
 
-    # Add your personal Zsh configuration below.
+      (lib.mkAfter ''
+        # Add personal Zsh commands below.
+        # The commented lines below are examples. Uncomment and adjust them if you want these preferences.
 
-    # The following commented block shows examples of personal preference settings.
-    # Uncomment and adjust it if you want these preferences.
-    #
+        # setopt auto_pushd
+        # setopt brace_ccl
+        # setopt globdots
+
+        # zstyle ':completion:*' completer _oldlist _expand _complete _correct _approximate
+        # zstyle ':completion:*' menu select=long
+
+        # alias -s py=python3
+      '')
+    ];
+
+    # Add personal Home Manager options for Zsh below.
+    # The commented lines below are examples. Uncomment and adjust them if you want these preferences.
+
     # autocd = true;
-    #
+
     # shellAliases = {
     #   python = "python3";
     # };
-    #
-    # initContent = lib.mkAfter ''
-    #   setopt auto_pushd
-    #   setopt brace_ccl
-    #   setopt globdots
-    #
-    #   zstyle ':completion:*' completer _oldlist _expand _complete _correct _approximate
-    #   zstyle ':completion:*' menu select=long
-    #
-    #   alias -s py=python3
-    # '';
   };
 }
