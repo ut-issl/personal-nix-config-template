2db65b4b399e75f062b7e91465cc71c6dd25b971
Riki Nakamura <riki.nakamura.edu@gmail.com>
2026-06-30 10:54:24 +0900

    refactor(zsh): use XDG path for zsh dotDir
    Replace the relative dotDir ".zsh" with an absolute XDG-based path
    ($XDG_CONFIG_HOME/zsh). Relative paths in programs.zsh.dotDir are
    deprecated in Home Manager, and the XDG location is preferred.

--- home-modules/user/zsh.nix
+++ home-modules/user/zsh.nix
@@ -12,7 +12,7 @@ in
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    dotDir = ".zsh";
+    dotDir = "${config.xdg.configHome}/zsh";
 
     # Command history. Edit any of these freely as you like.
     history = {
